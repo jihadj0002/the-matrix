4ad486a66ca5900701d51f1e2d1a61fc1e0263da
jihadj0002 <87649538+jihadj0002@users.noreply.github.com>
2025-09-17 19:07:12 +0600

    Updated and created roject

--- theMatrixAi/theMatrixAi/urls.py
+++ theMatrixAi/theMatrixAi/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for theMatrix project.
+URL configuration for theMatrixAi project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
--- theMatrixAi/theMatrixAi/wsgi.py
+++ theMatrixAi/theMatrixAi/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for theMatrix project.
+WSGI config for theMatrixAi project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "theMatrix.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "theMatrixAi.settings")
 
 application = get_wsgi_application()
