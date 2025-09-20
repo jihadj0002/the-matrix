e17a68b14a375b66ed44473972ec9f57c5a0a361
jihadj0002 <87649538+jihadj0002@users.noreply.github.com>
2025-09-21 01:32:41 +0600

    Updated dashbard and fixed url /

--- theMatrixAi/front/urls.py
+++ theMatrixAi/front/urls.py
@@ -6,9 +6,9 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("contact/", views.contact, name="contact"),
-    path("dashboard/", views.dashboard, name="dashboard"),
+    path("contact", views.contact, name="contact"),
+    path("dashboard", views.dashboard, name="dashboard"),
     
-    path("login/", views.login_view, name="login"),
-    path("logout/", views.logout_view, name="logout"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
 ]
